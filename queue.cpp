11a29ea44f101f05c2d35abb2696f3ebe4f02bc2
Shekhar <shekhar@shekhar.(none)>
2013-01-24 22:08:44 +0530

    First commit

---
+++ queue.cpp
@@ -0,0 +1,66 @@
+// Standard includes
+#include<cstdio>
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+#include<cstring>
+#include<limits.h>
+#include<string.h>
+
+//Data Structures
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<set>
+#include<map>
+#include<utility>
+#include<stack>
+#include<queue>
+using namespace std;
+
+#define FOR(i,a,b) for(int i= (int )a ; i < (int )b ; ++i)
+#define rep(i,n) FOR(i,0,n)
+#define INF INT_MAX
+#define all(x) x.begin(),x.end()
+#define LET(x,a) __typeof(a) x(a)
+#define IFOR(i,a,b) for(LET(i,a);i!=(b);++i)
+#define EACH(it,v) IFOR(it,v.begin(),v.end())
+#define pb push_back
+#define sz(x) int(x.size())
+#define mp make_pair
+#define fill(x,v) memset(x,v,sizeof(x))
+#define si(n) scanf("%d",&n)
+#define pi(n) printf("%d ",n)
+#define pil(n) printf("%d\n",n)
+#define sl(n) scanf("%lld",&n)
+#define sd(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+
+typedef pair<int,int> pr;
+typedef vector<int> vec;
+typedef vector<vec> matrix;
+typedef long long LL;
+#define mod 1000000007
+
+int main()
+{
+
+	int n,t,i,j;
+	string q;
+	cin >>n>>t;
+	cin>>q;
+	rep(j,t)
+	for(i=1;i<n;i++)
+	{
+		if(q[i]=='G' && q[i-1]=='B'){
+		q[i]='B';
+		q[i-1]='G';
+		i++;
+		}
+	}
+	
+	cout<<q<<endl;
+	
+return 0;
+}
+
